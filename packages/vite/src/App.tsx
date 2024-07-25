2d7fe2fbdb743ce7aa5bb251333d1bebb7842575
Conner Swenberg <conner.swenberg@coinbase.com>
2024-07-25 14:40:26 -0400

    Add transaction testing page to vite sample

--- packages/vite/src/App.tsx
+++ packages/vite/src/App.tsx
@@ -1,37 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Connect } from './components/Connect'
+import { Connect } from '@/components/Connect'
+import { TransactionTesting } from '@/components/TransactionTesting'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <div className='absolute top-0 right-0'>
+        <div className='absolute top-2 right-2'>
           <Connect />
         </div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <TransactionTesting />
     </>
   )
 }
--- packages/vite/src/main.tsx
+++ packages/vite/src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
-import OnchainProviders from './OnchainProviders.tsx'
 import '@coinbase/onchainkit/styles.css';
+
+import OnchainProviders from './OnchainProviders.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
