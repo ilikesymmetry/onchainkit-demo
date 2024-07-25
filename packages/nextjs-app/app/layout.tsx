680424f0c91768cf5a1ca10d8261f48cdde5bad9
Conner Swenberg <conner.swenberg@coinbase.com>
2024-07-25 14:19:58 -0400

    Add OCK wallet connection and v0 transaction page

--- packages/nextjs-app/app/layout.tsx
+++ packages/nextjs-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import OnchainProviders from "@/components/OnchainProviders";
+import '@coinbase/onchainkit/styles.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <OnchainProviders>
+          {children}
+        </OnchainProviders>
+      </body>
     </html>
   );
 }
