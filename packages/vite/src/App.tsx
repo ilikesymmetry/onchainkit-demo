a04204ec47c98df0ad82ea13bacff9ed7916f4c5
Conner Swenberg <conner.swenberg@coinbase.com>
2024-07-25 14:03:24 -0400

    Add mini app with OCK wallet connection

--- packages/vite/src/App.tsx
+++ packages/vite/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Connect } from './components/Connect'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <div className='absolute top-0 right-0'>
+          <Connect />
+        </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- packages/vite/src/main.tsx
+++ packages/vite/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import OnchainProviders from './OnchainProviders.tsx'
+import '@coinbase/onchainkit/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <OnchainProviders>
+      <App />
+    </OnchainProviders>
   </React.StrictMode>,
 )
