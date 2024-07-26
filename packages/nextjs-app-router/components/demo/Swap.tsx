3b3cc5f8c73351a4577f35eb5b1144894b6f77d6
Conner Swenberg <conner.swenberg@coinbase.com>
2024-07-26 16:02:56 -0400

    Update swap sample tokens

--- packages/nextjs-app-router/components/demo/Swap.tsx
+++ packages/nextjs-app-router/components/demo/Swap.tsx
@@ -14,23 +14,47 @@ function SwapComponent() {
   const { address } = useAccount();
   const {chainId} = useContext(AppContext)
  
-  const ETHToken: Token = {     
-    address: "",
-    chainId: 8453,
+  const degenToken: Token = {
+    name: 'DEGEN',
+    address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+    symbol: 'DEGEN',
     decimals: 18,
-    name: "Ethereum",
-    symbol: "ETH", 
-    };
- 
-  const USDCToken: Token = { 
-    address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    image:
+      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/3b/bf/3bbf118b5e6dc2f9e7fc607a6e7526647b4ba8f0bea87125f971446d57b296d2-MDNmNjY0MmEtNGFiZi00N2I0LWIwMTItMDUyMzg2ZDZhMWNm',
     chainId: 8453,
+  };
+
+  const ethToken: Token = {
+    name: 'ETH',
+    address: '',
+    symbol: 'ETH',
+    decimals: 18,
+    image:
+      'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+    chainId: 8453,
+  };
+
+  const usdcToken: Token = {
+    name: 'USDC',
+    address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+    symbol: 'USDC',
     decimals: 6,
-    name: "USDC",
-    symbol: "USDC",
-   };
- 
-  const swappableTokens: Token[] = [ETHToken, USDCToken];
+    image:
+      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+    chainId: 8453,
+  };
+
+  const wethToken: Token = {
+    name: 'Wrapped Ether',
+    address: '0x4200000000000000000000000000000000000006',
+    symbol: 'WETH',
+    decimals: 6,
+    image:
+      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/47/bc/47bc3593c2dec7c846b66b7ba5f6fa6bd69ec34f8ebb931f2a43072e5aaac7a8-YmUwNmRjZDUtMjczYy00NDFiLWJhZDUtMzgwNjFmYWM0Njkx',
+    chainId: 8453,
+  };
+
+  const swappableTokens = [degenToken, ethToken, usdcToken, wethToken];
  
   return (
     <div className='relative w-full h-full'>
@@ -47,14 +71,14 @@ function SwapComponent() {
         <SwapAmountInput
           label="Sell"
           swappableTokens={swappableTokens} 
-          token={ETHToken} 
+          token={ethToken} 
           type="from"
           /> 
         <SwapToggleButton /> 
         <SwapAmountInput
           label="Buy"
           swappableTokens={swappableTokens} 
-          token={USDCToken} 
+          token={usdcToken} 
           type="to"
           /> 
         <SwapButton /> 
